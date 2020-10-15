2565d4b1ad802a799d5ca6039116201f36686c1b
xingweizhu <etzhu192@hotmail.com>
2020-10-15 18:12:16 +0800

    remove ui folder

--- com.unity.uiwidgets/Runtime/ui/txt/utils.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Unity.UIWidgets.uiOld{
-    class Utils {
-        public static float PixelCorrectRound(float v) {
-            return Mathf.Round(v * Window.instance.devicePixelRatio) / Window.instance.devicePixelRatio;
-        }
-    }
-}
