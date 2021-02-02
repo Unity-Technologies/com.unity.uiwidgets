7ba4ae123f7eefde962f9bae631862545e5e6b60
xingweizhu <etzhu192@hotmail.com>
2021-02-02 12:40:06 +0800

    add all ok material samples

---
+++ Samples/UIWidgetsSamples_2019_4/Assets/MaterialSample/MaterialThemeSample.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using uiwidgets;
+using Unity.UIWidgets.animation;
+using Unity.UIWidgets.engine;
+using Unity.UIWidgets.engine2;
+using Unity.UIWidgets.material;
+using Unity.UIWidgets.painting;
+using Unity.UIWidgets.rendering;
+using Unity.UIWidgets.service;
+using Unity.UIWidgets.ui;
+using Unity.UIWidgets.widgets;
+using UnityEngine;
+using Image = Unity.UIWidgets.widgets.Image;
+using ui_ = Unity.UIWidgets.widgets.ui_;
+
+namespace UIWidgetsSample {
+    public class MaterialThemeSample: UIWidgetsPanel {
+        
+        protected override void main()  {
+            ui_.runApp(new MaterialApp(
+                home: new MaterialThemeSampleWidget(),
+                darkTheme: new ThemeData(primaryColor: Colors.black26)
+            ));
+        }
+        
+        protected new void OnEnable() {
+            base.OnEnable();
+        }
+    }
+
+    public class MaterialThemeSampleWidget: StatefulWidget {
+        public override State createState() {
+            return new _MaterialThemeSampleWidgetState();
+        }
+    }
+
+    class _MaterialThemeSampleWidgetState : State<MaterialThemeSampleWidget> {
+        public override Widget build(BuildContext context) {
+            return new Theme(
+                data: new ThemeData(
+                    appBarTheme: new AppBarTheme(
+                        color: Colors.purple
+                    ),
+                    bottomAppBarTheme: new BottomAppBarTheme(
+                        color: Colors.blue
+                    ),
+                    cardTheme: new CardTheme(
+                        color: Colors.red,
+                        elevation: 2.0f
+                    )
+                ),
+                child: new Scaffold(
+                    appBar: new AppBar(title: new Text("Test App Bar Theme")),
+                    body: new Center(
+                        child: new Card(
+                            shape: new RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(5.0f)
+                            ),
+                            child: new Container(
+                                height: 250,
+                                child: new Column(
+                                    children: new List<Widget> {
+                                        new Text("Card Theme")
+                                    }
+                                )
+                            )
+                        )
+                    ),
+                    bottomNavigationBar: new BottomAppBar(
+                        child: new Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: new List<Widget> {
+                            new IconButton(icon: new Icon(Unity.UIWidgets.material.Icons.menu), onPressed: () => { }),
+                            new IconButton(icon: new Icon(Unity.UIWidgets.material.Icons.account_balance), onPressed: () => { })
+                        })
+                    )
+                )
+            );
+        }
+    }
+}
---
+++ Samples/UIWidgetsSamples_2019_4/Assets/MaterialSample/TableSample.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using uiwidgets;
+using Unity.UIWidgets.engine2;
+using Unity.UIWidgets.foundation;
+using Unity.UIWidgets.material;
+using Unity.UIWidgets.painting;
+using Unity.UIWidgets.rendering;
+using Unity.UIWidgets.ui;
+using Unity.UIWidgets.widgets;
+using UnityEngine;
+using Color = Unity.UIWidgets.ui.Color;
+using ui_ = Unity.UIWidgets.widgets.ui_;
+
+namespace UIWidgetsSample {
+    
+    public class TableSample : UIWidgetsPanel {
+
+        protected override void main() {
+            ui_.runApp(new MaterialApp(
+                showPerformanceOverlay: false,
+                home: new TableWidget()));
+        }
+
+        protected new void OnEnable() {
+            base.OnEnable();
+        }
+    }
+    
+    public class TableWidget : StatelessWidget {
+        public TableWidget(Key key = null) : base(key) {
+        }
+
+        public override Widget build(BuildContext context) {
+            return new Scaffold(
+                body: new Table(
+                    children: new List<TableRow> {
+                        new TableRow(
+                            decoration: new BoxDecoration(color: Colors.blue),
+                            children: new List<Widget> {
+                                new Text("item 1"),
+                                new Text("item 2")
+                            }
+                        ),
+                        new TableRow(children: new List<Widget> {
+                                new Text("item 3"),
+                                new Text("item 4")
+                            }
+                        )
+                    },
+                    defaultVerticalAlignment: TableCellVerticalAlignment.middle));
+        }
+    }
+}
