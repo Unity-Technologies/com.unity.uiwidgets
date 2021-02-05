586de84845a5121f1310b7e3a26b1ad6ea85609d
Shiyun Wen <shiyun.wen@unity3d.com>
2021-02-05 17:03:19 +0800

    add demow

--- Samples/UIWidgetsSamples_2019_4/Assets/UIWidgetsGallery/demo/shrine_demo.cs
+++ Samples/UIWidgetsSamples_2019_4/Assets/UIWidgetsGallery/demo/shrine_demo.cs
@@ -5,11 +5,15 @@ using Unity.UIWidgets.widgets;
 
 namespace UIWidgetsGallery.demo
 {
-    class ShrineDemo : StatelessWidget {
-    public ShrineDemo(Key key = null) : base(key: key){}
+    class ShrineDemo : StatelessWidget 
+    {
+        public ShrineDemo(Key key = null) : base(key: key){}
 
-    public static readonly string routeName = "/shrine"; // Used by the Gallery app.
+        public static readonly string routeName = "/shrine";
 
-    public override Widget build(BuildContext context) => new ShrineApp();
+        public override Widget build(BuildContext context)
+        {
+            return new ShrineApp();
+        }
     }
 }
