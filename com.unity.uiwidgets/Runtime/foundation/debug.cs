4cd03ca33c945849b4f41fba2c7818153d188e6d
siyao <tangerine_juice@live.com>
2021-02-22 16:28:40 +0800

    add debug log config

--- com.unity.uiwidgets/Runtime/foundation/debug.cs
+++ com.unity.uiwidgets/Runtime/foundation/debug.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.Linq;
+using Unity.UIWidgets.engine2;
 using Unity.UIWidgets.painting;
 using Unity.UIWidgets.ui;
 using Canvas = Unity.UIWidgets.ui.Canvas;
@@ -16,14 +17,14 @@ namespace Unity.UIWidgets.foundation {
 
         [Conditional("UNITY_ASSERTIONS")]
         public static void assert(Func<bool> result, Func<string> message = null) {
-            if (!result()) {
+            if (UIWidgetsPanel.ShowDebugLog && !result()) {
                 throw new AssertionError(message != null ? message() : "");
             }
         }
 
         [Conditional("UNITY_ASSERTIONS")]
         public static void assert(bool result, Func<string> message = null) {
-            if (!result) {
+            if (UIWidgetsPanel.ShowDebugLog && !result) {
                 throw new AssertionError(message != null ? message() : "");
             }
         }
