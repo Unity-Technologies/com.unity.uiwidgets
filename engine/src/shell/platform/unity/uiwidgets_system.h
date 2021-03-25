a586a91ed2ec6e4e13e72de3a59ed4382b6bdc30
xingwei.zhu <etzhu192@hotmail.com>
2021-03-25 11:27:04 +0800

    fix typo

--- engine/src/shell/platform/unity/uiwidgets_system.h
+++ engine/src/shell/platform/unity/uiwidgets_system.h
@@ -4,7 +4,7 @@
     #ifdef TARGET_OS_IOS
         #include "shell/platform/unity/darwin/ios/uiwidgets_system.h"
     #else
-        #include "shell/platform/unity/darwin/mac/uiwidgets_system.h"
+        #include "shell/platform/unity/darwin/macos/uiwidgets_system.h"
     #endif
 #elif _WIN64
     #include "shell/platform/unity/windows/uiwidgets_system.h"
