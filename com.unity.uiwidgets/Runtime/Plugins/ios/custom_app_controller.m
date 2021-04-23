2e884057f2a177d88482383b091af4b00d7c496d
guanghuispark <guanghui.zhang@unity3d.com>
2021-04-23 18:32:49 +0800

    update CustomAppController

--- com.unity.uiwidgets/Runtime/Plugins/ios/custom_app_controller.m
+++ com.unity.uiwidgets/Runtime/Plugins/ios/custom_app_controller.m
@@ -2,25 +2,20 @@
 
 #import "UnityAppController.h"
 #import <TargetConditionals.h>
+#import "custom_app_controller.h"
 
 extern void UnityPluginLoad(struct IUnityInterfaces *interfaces);
 extern void UnityPluginUnload(void);
 
 #pragma mark - App controller subclasssing
 
-@interface CustomAppController : UnityAppController
-{
-}
-- (void)shouldAttachRenderDelegate;
-@end
-
-@implementation CustomAppController
+@implementation CustomUIWidgetsAppController
 - (void)shouldAttachRenderDelegate;
 {
     UnityRegisterRenderingPluginV5(&UnityPluginLoad, &UnityPluginUnload);
 }
 @end
 
-IMPL_APP_CONTROLLER_SUBCLASS(CustomAppController);
+IMPL_APP_CONTROLLER_SUBCLASS(CustomUIWidgetsAppController);
 
 #endif 
